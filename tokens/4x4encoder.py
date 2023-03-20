183b83af582eb421a8298dac46b42fad68bed075
iTechnicals <61851185+iTechnicals@users.noreply.github.com>
2023-03-20 13:35:59 +0000

    Update 4x4encoder.py
    Patched bug

--- tokens/4x4encoder.py
+++ tokens/4x4encoder.py
@@ -8,7 +8,7 @@ def encode(num):
             ["0", "0", "0", "0"]]
     for i, j in enumerate(binary[8:]):
         data[3][i] = j
-    for i, j in enumerate(binary[:3]):
+    for i, j in enumerate(binary[:4]):
         data[0][i] = j
     data[1][0] = binary[4]; data[1][3] = binary[5]
     data[2][0] = binary[6]; data[2][3] = binary[7]
